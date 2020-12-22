ec9c512729bba9f4d189766d3232410ffe788c2d
OlahTamas <olah.tamas@datalytix.tech>
2020-12-22 10:40:32 +0000

    Working on child component manager

---
+++ resources/js/components/ChildItemManager.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="w-full flex flex-col">
+        <div class="text-xl font-bold w-full py-4"
+             v-html="title"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            operationsUrl: {type: String},
+            title: {type: String}
+        },
+        data: function () {
+            return {}
+        },
+        mounted() {
+        },
+        methods: {},
+        computed: {},
+        watch: {}
+
+    }
+</script>
