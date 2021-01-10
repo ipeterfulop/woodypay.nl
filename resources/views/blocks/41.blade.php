56115aa369cb6a4fb733c6311ff088e88353e9a0
OlahTamas <olah.tamas@datalytix.tech>
2021-01-10 10:06:39 +0000

    Fixed feature list

--- resources/views/blocks/41.blade.php
+++ resources/views/blocks/41.blade.php
@@ -5,7 +5,11 @@
         <div class="p-4 flex flex-row flex-wrap items-start justify-start">
             @foreach($block->getItemsContainer()->items as $index => $item)
                 <div class="w-full md:w-1/2 lg:w-1/3 flex flex-col items-start justify-start p-4">
-                    <img src="{{ $item->image_url }}" class="h-16">
+                    @if($item->fa_icon_classes != null)
+                        <i class="fa {{ $item->fa_icon_classes }}"></i>
+                    @else
+                        <img src="{{ $item->image_url }}" class="h-16">
+                    @endif
                     <h3>{{ $item->title_translated }}</h3>
                     <div>{!! $item->content_translated !!}</div>
                 </div>
